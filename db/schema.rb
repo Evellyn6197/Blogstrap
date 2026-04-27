608c1ad6031845d7a6b45afe27696c9185d4a48e
Evellyn Gomes <evellyn.ferreira@sefin.fortaleza.ce.gov.br>
2026-04-27 13:20:26 -0300

    Gera um scaffold para Category.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_04_22_193747) do
+ActiveRecord::Schema[7.2].define(version: 2026_04_27_161748) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
