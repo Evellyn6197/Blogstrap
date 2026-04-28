b6641101dbfe2636cec0089b12e801f2100f3df9
Evellyn Gomes <evellyn.ferreira@sefin.fortaleza.ce.gov.br>
2026-04-28 14:14:34 -0300

    Faz com que um Article criado seja associado ao usuário logado.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,11 +15,11 @@ end
   end
 
   def new
-      @article = Article.new
+      @article = current_user.articles.new
   end
 
   def create
-      @article = Article.new(article_params)
+      @article = current_user.articles.new(article_params)
 
       if @article.save
           redirect_to @article, notice: "Article was successfully created."
