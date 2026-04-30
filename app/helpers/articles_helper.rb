10a821e23c5b9530edf7e1a4794ccc08c1539b30
Evellyn Gomes <evellyn.ferreira@sefin.fortaleza.ce.gov.br>
2026-04-30 13:22:46 -0300

    Melhora identação

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,6 +1,5 @@
 module ArticlesHelper
-    def
-        format_date(datetime)
-            datetime.strftime("%B %e, %Y")
+    def format_date(datetime)
+        datetime.strftime("%B %e, %Y")
     end
 end
